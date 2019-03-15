11a08a4bf42c8baa6fe5fa1b51b72c9b07d47040
Rafael Mariscal <dominicmariscal78@gmail.com>
2019-03-15 16:42:07 -0400

    Added data for mainblock.vue, app.vue and main.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MainBlock />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainBlock from "./components/MainBlock.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    MainBlock
   }
 };
 </script>
